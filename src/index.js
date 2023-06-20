842038f0515baacea0703dab6b96f2af7473da9c
seandhoey <seanhoey@live.ca>
2023-06-19 21:04:00 -0600

    Initial

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import initScrollReveal from "./scripts/scrollReveal.js";
+import initTiltEffect from "./scripts/tiltAnimation.js";
+import { targetElements, defaultProps } from "./data/scrollRevealConfig.js";
+
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
